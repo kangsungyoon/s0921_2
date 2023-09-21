9b1cfabb20ecaddd144be4398da1bd6b138c96d7
kangsungyoon <ksy20051001@gmail.com>
2023-09-21 14:18:13 +0900

    이미지 생성함수 개선

--- main.js
+++ main.js
@@ -10,11 +10,32 @@
 // 위의 작업을 100번 반복을 돌리면서
 // append로 이미지 요소 반복추가
 
+// 백분율 구하는 공식
+// 현재 수치값 / 전체수치값 * 100 (백분율)
+// 현재 수치값 / 전체수치값 * 200 (이백분율)
+
 const section = document.querySelector('section');
 
-const img = document.createElement('img');
-const src = document.createAttribute('src');
-src.value = 'img/pic100.jpg';
-img.setAttributeNode(src);
+window.addEventListener('mousemove', (e) => {
+	const curPos = e.pageX;
+	const wid = window.innerWidth;
+	const percent = parseInt((curPos / wid) * 200);
+	console.log(percent);
 
-section.append(img);
+	//parseInt(숫자) : 실수에서 소수점 아래를 버려서 정수변환
+	//parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
+});
+
+// 인수로 갯수를 받아서 동적으로 img생성해 주는 함수
+
+function createImgs(target, num) {
+	for (let i = 0; i < num; i++) {
+		const img = document.createElement('img');
+		const src = document.createAttribute('src');
+		src.value = `img/pic${i}.jpg`;
+		img.setAttributeNode(src);
+		target.append(img);
+	}
+
+	target.querySelectorAll('img');
+}
